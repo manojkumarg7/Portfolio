8bc3b0312c58e97b730076b0a8a40fcc913708ed
manojkumarg7 <manojkumargowdru7@gmail.com>
2024-04-15 17:15:58 +0530

    added certificates section added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
- 
+// window.addEventListener("scroll", reveal);
+
+// function reveal() {
+//   var reveals = document.querySelectorAll(".reveal");
+//   for (var i = 0; i < reveals.length; i++) {
+//     var windowHeight = window.innerHeight;
+//     var revealtop = reveals[i].getBoundingClientRect().top;
+//     var revealPoint = 150;
+//     if (revealtop < windowHeight - revealPoint) {
+//       reveals[i].classList.add(".active");
+//     } else {
+//       reveals[i].classList.remove("active");
+//     }
+//   }
+// }
